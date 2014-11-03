e5123ee5eb8b764b97483c3959d96f7fba8abb2a
Dwayne Macgowan <dwaynemac@gmail.com>
2014-11-03 15:08:21 -0300

    secure doorbell form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def generate_secure_doorbell_signature()
+    jsonp_secret = ENV['doorbellio_key']
+    timestamp = Time.now.to_i
+    token = SecureRandom.hex(50)
+
+    signature = OpenSSL::HMAC.hexdigest(
+                OpenSSL::Digest::Digest.new('sha256'),
+                jsonp_secret,
+                '%s%s' % [timestamp, token])
+
+    return "timestamp: #{timestamp}, token: '#{token}', signature: '#{signature}', ".html_safe
+  end
 end
